d810bd57f8221b39391f9d2c8b89b3a5e59ed4d0
jribon51 <151412716+jribon51@users.noreply.github.com>
2024-05-20 01:50:02 +0000

    solo la parte que se hizo en clases

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User,Planets
 #from models import Person
 
 app = Flask(__name__)
@@ -38,12 +38,40 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    #select * from user;
+    all_users = User.query.all()
+    user_serialized=[]
+    for user in all_users:
+        user_serialized.append(user.serialize())
+    print(user_serialized)
+    return jsonify({"data":user_serialized}), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user/<int:id>',methods=["GET"])
+def get_single_user(id):
+    single_user = User.query.get(id)
+    if single_user==None:
+        return jsonify({"msg":"No se encontro el id {} del usuario".format(id)}),400
+    return jsonify({"data":single_user.serialize()}),200
 
-    return jsonify(response_body), 200
+@app.route("/planet", methods=["POST"])
+def new_planet():
+    body=request.get_json(silent=True)
+    print(type(body),body)
+    if body is None:
+        return jsonify({"msg":"debes enviar informacion en el body"}),400
+    if "name" not in body:
+        return jsonify({"msg":"el campo name es obligatorio"}),400
+    if "population" not in body:
+        return jsonify({"msg":"el campo population es obligatorio"}),400
+    new_planet = Planets()
+    new_planet.name = body["name"]
+    new_planet.population = body["population"]
+    db.session.add(new_planet)
+    db.session.commit()
+    return jsonify({"msg":"nuevo planeta creado","data":new_planet.serialize()}),201
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
